04d13883213d243c09ffb11260c4bbbacaffbe89
Alexander <alexander00082@gmail.com>
2024-12-14 23:40:07 +0100

    Abstract Violation Class

--- preciceconfigchecker/rule.py
+++ preciceconfigchecker/rule.py
@@ -1,15 +1,13 @@
 from abc import ABC, abstractmethod
 from typing import List
 from severity import Severity
+from violation import Violation
 
 
 class Rule(ABC):
     """
     Abstract Class 'Rule'. Checking a 'Rule' for violations and producing formatted output.
     """
-
-    violation_counter:int = 0
-    """Static Attribute: Do not use 'self.numbers', use 'Rule.numbers'"""
 
     def __init__(self, severity:Severity, problem:str) -> None:
         """
@@ -21,9 +19,7 @@ class Rule(ABC):
         """
         self.severity = severity
         self.problem = problem
-        self.rule:str
-        self.formatted_violations:List[str] = []
-        self.violations:List[tuple] = []
+        self.violations:List[Violation] = []
         rules.append(self)
 
     @abstractmethod
@@ -31,39 +27,7 @@ class Rule(ABC):
         """
         @abstractmethod: Defines how a 'Rule' should be checked
 
-        Tip: Use 'self.violations.append()' to save the results directly.
-        """
-        pass
-
-    @abstractmethod
-    def format_explanation(self, data:tuple) -> str:
-        """
-        @abstractmethod: Formats the explanation of a violation
-
-        Args:
-            data (tuple): to use for formatting
-
-        Returns:
-            str: formatted
-
-        Tip: When overwriting, multiple arguments can be passed directly, rather than a tuple of arguments.
-        Tip: Define the abstract 'check_method' beforehand. Define the types of the tuple there and use these types for the arguments here.
-        """
-        pass
-    
-    @abstractmethod
-    def format_possible_solutions(self, data:tuple) -> List[str]:
-        """
-        @abstractmethod: Formats multiple possible solutions to a violation
-
-        Args:
-            data (tuple): to use for formatting
-
-        Returns:
-            List[str]: of formatted possible solutions
-
-        Tip: When overwriting, multiple arguments can be passed directly, rather than a tuple of arguments.
-        Tip: Define the abstract 'check_method' beforehand. Define the types of the tuple there and use these types for the arguments here.
+        Tip: Use 'self.violations.append(self.MyViolation(...))' to save the results directly. MyViolation should be an inner class of type Violation.
         """
         pass
     
@@ -74,42 +38,18 @@ class Rule(ABC):
         Returns:
             bool: TRUE if there are no violations after the check
         """
-        return (len(self.violations) == 0)
-    
-    def can_print(self) -> bool:
-        """
-        Shows if the 'Rule' can print formatted violations
-
-        Returns:
-            bool: TRUE if formatted violations are available
-        """
-        return (len(self.formatted_violations) > 0)
-
-    def format_result(self) -> None:
-        """
-        If the 'Rule' is not satisfied, an output is formatted
-        """
-        if self.satisfied():
-            return
-        self.rule = f"[{self.severity.value}]: {self.problem}"
-        for data in self.violations:
-            explanation:str = self.format_explanation(*data)
-            possible_solutions:List[str] = self.format_possible_solutions(*data)
-            Rule.violation_counter += 1
-            violation:str = f"({Rule.violation_counter:3}.): {explanation}"
-            for possible_solution in possible_solutions:
-                violation += f"\n\t- {possible_solution}"
-            self.formatted_violations.append(violation)
+        return (self.violations.__len__() == 0)
 
     def print_result(self) -> None:
         """
-        If the 'Rule' has formatted output, it will be printed
+        If the 'Rule' has violations, these will be printed.
         """
-        if not self.can_print():
+        if self.satisfied():
             return
-        print(self.rule)
-        for violation in self.formatted_violations:
-            print(violation)
+        print(f"[{self.severity.value}]: {self.problem}")
+        for violation in self.violations:
+            formatted_violation = violation.format()
+            print(formatted_violation)
 
 
 
@@ -125,18 +65,10 @@ def check_all_rules() -> None:
     for rule in rules:
         rule.check()
 
-def format_all_results() -> None:
+def print_all_results() -> None:
     """
-    Formats the existing violations for all rules
+    Prints all existing violations of all rules
     """
     for rule in rules:
         if not rule.satisfied():
-            rule.format_result()
-
-def print_all_results() -> None:
-    """
-    Prints all existing formatted violations of all rules
-    """
-    for rule in rules:
-        if rule.can_print():
             rule.print_result()
